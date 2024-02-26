acb859a9c6bb7a0c5bd98ce7175bb11e20a5aa2e
dave-ad <davidaderibigbe47@gmail.com>
2024-02-26 05:13:14 +0100

    complete arithmetic op

--- Calculator/MainWindow.xaml.cs
+++ Calculator/MainWindow.xaml.cs
@@ -15,16 +15,96 @@ using System.Windows.Shapes;
 
 namespace Calculator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private double currentNumber = 0;
+        private double result = 0;
+        private char? currentOperation = null;
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private void btnNumber_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                string number = button.Content.ToString();
+                if (lblResult.Content.ToString() == "0")
+                    lblResult.Content = number;
+                else
+                    lblResult.Content += number;
+            }
+        }
 
+        private void clearBtn(object sender, RoutedEventArgs e)
+        {
+            ClearControls();
+        }
+
+        private void btnOperation_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender is Button button)
+            {
+                if (currentOperation != null)
+                {
+                    PerformOperation();
+                }
+
+                currentNumber = double.Parse(lblResult.Content.ToString());
+                currentOperation = button.Content.ToString()[0];
+
+                //string operation = button.Content.ToString();
+                //operationsLbl.Content += lblResult.Content + " " + operation + " ";
+
+                lblResult.Content = "0";
+                operationsLbl.Content += currentNumber.ToString() + " " + currentOperation + " ";
+            }
+        }
+
+        private void PerformOperation()
+        {
+            if (currentOperation != null)
+            {
+                double number = double.Parse(lblResult.Content.ToString());
+                switch (currentOperation)
+                {
+                    case '+':
+                        result = currentNumber + number;
+                        break;
+                    case '-':
+                        result = currentNumber - number;
+                        break;
+                    case '*':
+                        result = currentNumber * number;
+                        break;
+                    case '/':
+                        result = currentNumber / number;
+                        break;
+                }
+                lblResult.Content = result.ToString();
+                currentNumber = result;
+            }
+        }
+
+        private void btnEquals_Click(object sender, RoutedEventArgs e)
+        {
+            PerformOperation();
+            currentOperation = null;
+        }
+
+        private void ClearControls()
+        {
+            lblResult.Content = "0";
+            operationsLbl.Content = "";
+        }
+        
+        private void showOpResult(object sender, RoutedEventArgs e)
+        {
+            PerformOperation();
+            operationsLbl.Content += lblResult.Content + " = ";
+            currentOperation = null;
+        }
     }
 }
